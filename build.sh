f890cf822f74241a868aef9fe1a71b53da80d5d4
linepro6 <solaryy.sr@gmail.com>
2022-05-23 01:04:32 +0800

    fix ci

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ dotnet build -c Release \
     -p:OutputPath=../output/bin/
 
 # build source code zip
-git archive --format zip --output output/source.zip HEAD
+# git archive --format zip --output output/source.zip HEAD
 
-cat output/bin-noreactor/Mini.RegionInstall.dll output/source.zip >output/Mini.RegionInstall-NoReactor.dll
-cat output/bin/Mini.RegionInstall.dll output/source.zip >output/Mini.RegionInstall.dll
+# cat output/bin-noreactor/Mini.RegionInstall.dll output/source.zip >output/Mini.RegionInstall-NoReactor.dll
+# cat output/bin/Mini.RegionInstall.dll output/source.zip >output/Mini.RegionInstall.dll
 #cp ./Mini.RegionInstall/bin-reactor/Mini.RegionInstall.dll ./Mini.RegionInstall/bin-reactor/Mini.RegionInstall_Reactor.dll
 # Artifact can be found in Mini.RegionInstall/bin-reactor/Mini.RegionInstall_Reactor.dll
